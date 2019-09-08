a7d5ebe132d7aec5deacfdd9db54b8ca27493fed
Spencer Harston <spencer314@gmail.com>
2019-09-08 08:08:53 -0600

    Adjust vibration length

--- script/script.js
+++ script/script.js
@@ -11,14 +11,14 @@ saveButton.addEventListener('click', saveCount);
 resetButton.addEventListener('click', resetCount);
 
 function increaseCount(){
-    window.navigator.vibrate(200);
+    window.navigator.vibrate(75);
     totalCount++;
     console.log(totalCount);
     updateCount();
 }
 
 function decreaseCount(){
-    window.navigator.vibrate(200);
+    window.navigator.vibrate(75);
     if(totalCount > 0) totalCount--;
     console.log(totalCount);
     updateCount();
