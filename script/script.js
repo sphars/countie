59676f8e0d6f51110f0501548f88d04cfdce8ebc
Spencer Harston <spencer314@gmail.com>
2019-09-08 20:08:08 -0600

    Add save functionality. Closes #1

--- script/script.js
+++ script/script.js
@@ -1,39 +1,86 @@
+//Buttons
+const count = document.querySelector("#count");
+const increaseBtn = document.querySelector('#increase');
+const decreaseBtn = document.querySelector('#decrease');
+const saveBtn = document.querySelector('#save');
+const resetBtn = document.querySelector('#reset');
+const resetAllBtn = document.querySelector('#resetAll');
+const countUl = document.querySelector('ul');
+
 let totalCount = 0;
 
-let increaseButton = document.getElementById('increase');
-let decreaseButton = document.getElementById('decrease');
-let saveButton = document.getElementById('save');
-let resetButton = document.getElementById('reset');
+//Manage counts
 
-increaseButton.addEventListener('click', increaseCount);
-decreaseButton.addEventListener('click', decreaseCount);
-saveButton.addEventListener('click', saveCount);
-resetButton.addEventListener('click', resetCount);
+//get the records from storage
+let countRecordsArray = localStorage.getItem('counts') ? JSON.parse(localStorage.getItem('counts')) : [];
 
+//store the records back in storage
+localStorage.setItem('counts', JSON.stringify(countRecordsArray));
+
+//fecth data for local modifications
+const countData = JSON.parse(localStorage.getItem('counts'));
+
+//create an li entry
+const liMaker = record =>{
+    const li = document.createElement('li');
+    const recordDate = new Date(record.date).toLocaleString();
+    li.textContent = recordDate + ": " + record.count + ": " + record.note;
+    countUl.appendChild(li);
+}
+
+//loop through count array and make an li for each
+countData.forEach(item => {
+    liMaker(item);
+});
+
+//Functions
 function increaseCount(){
     window.navigator.vibrate(25);
     totalCount++;
-    console.log(totalCount);
     updateCount();
 }
 
 function decreaseCount(){
     window.navigator.vibrate(25);
     if(totalCount > 0) totalCount--;
-    console.log(totalCount);
     updateCount();
 }
 
 function updateCount(){
-    let countDisplay = document.getElementById("count");
-    countDisplay.innerHTML = totalCount.toString();
+    count.textContent = totalCount.toString();
 }
 
+//saves the current count, creates a record, adds to the storage
 function saveCount(){
-    localStorage.setItem('totalCount', totalCount);
+
+    //creates a record object
+    const countRecord = {
+        date: new Date(),
+        count: totalCount,
+        note: prompt("Type a note")
+    }
+
+    countRecordsArray.push(countRecord);
+    localStorage.setItem('counts', JSON.stringify(countRecordsArray));
+    liMaker(countRecord);
+    resetCount();
 }
 
 function resetCount(){
     totalCount = 0;
     updateCount();
 }
+
+function resetAll(){
+    localStorage.clear();
+    while (countUl.firstChild){
+        countUl.removeChild(countUl.firstChild)
+    }
+}
+
+//Event listeners
+increaseBtn.addEventListener('click', increaseCount);
+decreaseBtn.addEventListener('click', decreaseCount);
+saveBtn.addEventListener('click', saveCount);
+resetBtn.addEventListener('click', resetCount);
+resetAllBtn.addEventListener('click', resetAll);
